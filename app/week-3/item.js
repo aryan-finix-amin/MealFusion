17c76e98867dc806db03061fb29714e9e2425241
aryan-finix-amin <aryanfinixamin1104@gmail.com>
2024-06-04 19:09:30 -0600

    Assignment

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
-export default function item({name, quantity, category}) {
-    return (
-        <li className="flex p-4 bg-purple-100 shadow-md rounded-lg my-2">
-            <div className="flex flex-col">
-                <span className="font-bold text-lg">{name}</span>
-                <span className="text-gray-500">{quantity}</span>
-                <span className="text-orange-500 font-semibold">{category}</span>
-            </div>
-        </li>
-    );
+export default function Item({ name, quantity, category }) {
+  return (
+    <li className="flex p-4 bg-purple-100 shadow-md rounded-lg my-2">
+      <div className="flex flex-col">
+        <span className="font-bold text-lg">{name}</span>
+        <span className="text-gray-500"> Buy {quantity}</span>
+        <span className="text-orange-500 font-semibold"> in {category}</span>
+      </div>
+    </li>
+  );
 }
--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -23,17 +23,47 @@ export default function NewItem() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name:</label>
+    <form onSubmit={handleSubmit} className="min-h-screen bg-purple-200">
+      <div className="bg-purple-500 mx-auto">
         <input
+          className="m-4 rounded-md "
           type="text"
           placeholder="Item Name"
           value={name}
           onChange={(e) => setName(e.target.value)}
           required
         />
+        <input
+          className="m-4 rounded-md"
+          type="number"
+          min="1"
+          max="99"
+          value={quantity}
+          onChange={(e) => setQuantity(parseInt(e.target.value))}
+          required
+        />
+        <select
+          className="m-4 rounded-md"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          required
+        >
+          <option>Produce</option>
+          <option>Dairy</option>
+          <option>Bakery</option>
+          <option>Meat</option>
+          <option>Frozen Foods</option>
+          <option>Canned Goods</option>
+          <option>Dry Goods</option>
+          <option>Beverages</option>
+          <option>Snacks</option>
+          <option>Household</option>
+          <option>Other</option>
+        </select>
       </div>
+      <button className="container bg-purple- 00 shadow-md ml-10 mt-10 rounded-lg w-20 h-10 mx-auto hover:text-blue-700">
+        Submit
+      </button>
     </form>
   );
 }
